6fdd2322d65450861a06c295cd2a48fe1e1b8e90
Aryan Negi <aryannegi526@gmail.com>
2025-08-04 20:12:26 +0530

    responsiveness & footer added

---
+++ script.js
@@ -0,0 +1,10 @@
+//caption is vanishing on ratio {carousel caption - <522.65 x 48} 
+//>> changing the carousel div according to mobile screen & text should be visible 
+let caption = document.querySelector ("carousel-caption");
+
+
+
+
+
+// background image of anime are not properly showing having the ratio of mobile screen BG img - 387.2 x 769.65 
+// adding custom image in folder /image that is on change the image should be displayed in 9:16 ratio / 387.2 x 769.65 
